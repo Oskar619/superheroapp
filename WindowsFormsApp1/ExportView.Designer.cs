95afeb36785a5fed91e4a6ddd05c4fb6561ae636
Oscar Obeso <ozkar619@gmail.com>
2018-04-08 20:16:22 -0500

    Changed view button.

--- WindowsFormsApp1/ExportView.Designer.cs
+++ WindowsFormsApp1/ExportView.Designer.cs
@@ -29,33 +29,21 @@
         private void InitializeComponent()
         {
             this.txtExportData = new System.Windows.Forms.RichTextBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtExportData
             // 
             this.txtExportData.Location = new System.Drawing.Point(12, 12);
             this.txtExportData.Name = "txtExportData";
-            this.txtExportData.Size = new System.Drawing.Size(981, 482);
+            this.txtExportData.Size = new System.Drawing.Size(981, 521);
             this.txtExportData.TabIndex = 0;
             this.txtExportData.Text = "";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(478, 510);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Save as file";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // ExportView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1005, 545);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.txtExportData);
             this.Name = "ExportView";
             this.Text = "Export View";
@@ -66,6 +54,5 @@
         #endregion
 
         private System.Windows.Forms.RichTextBox txtExportData;
-        private System.Windows.Forms.Button button1;
     }
 }
